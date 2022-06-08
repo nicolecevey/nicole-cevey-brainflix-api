4b21a3d3b4a3aaa49d12a8fa3c218aeeaae8b076
Nicole Cevey <nicole.cevey@gmail.com>
2022-06-08 17:17:39 -0400

    Added feature to upload video to api

--- index.js
+++ index.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const cors = require("cors");
-const videos = require("./routes/videos");
 
+const videos = require("./routes/videos");
 const app = express();
 
 app.use(cors());
+
+app.use(express.static("./public/images"));
 
 app
 	.get("/", (req, res) => {
--- routes/videos.js
+++ routes/videos.js
@@ -2,15 +2,43 @@ const express = require("express");
 const router = express.Router();
 const fs = require('fs');
 
+const { v4: uuid} = require("uuid");
+const bodyParser = require("body-parser")
+
 const videosFilePath = "./data/videos.json";
+
+router.use(bodyParser.json())
+
 const getVideos = () => {
     return JSON.parse(fs.readFileSync(videosFilePath));
 }
 
 router.get('/videos', (req, res) => {
-    let videoDetails = getVideos();
-    res.send(videoDetails)
+        let videoDetails = getVideos();
+        res.send(videoDetails)
 });
+
+router.post('/videos', (req, res) => {
+    const { title, description } = req.body;
+    let videoDetails = getVideos();
+    const video = {
+        title: title,
+        channel: "Oliver's Adventures",
+        image: "http://localhost:8085/Upload-video-preview.jpg",
+        description: description,
+        views: "0",
+        likes: "0",
+        duration: "10:14",
+        video: "https://project-2-api.herokuapp.com/stream",
+        timestamp: Date.now(),
+        comments: [],
+        id: uuid()
+    }
+  
+    videoDetails.push(video)
+    fs.writeFileSync("./data/videos.json", JSON.stringify(videoDetails))
+    res.send(video);
+})
 
 router.get('/videos/:id', (req, res) => {  
     const requestedId = req.params.id;
