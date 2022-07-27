17d46d580e40023ac96af57197db3676a5fc6d4e
Nicole Cevey <nicole.cevey@gmail.com>
2022-07-27 15:25:17 -0400

    Adding procfile to deploy to heroku

--- index.js
+++ index.js
@@ -10,15 +10,12 @@ const { PORT, BACKEND_URL } = process.env;
 app.use(cors());
 app.use(express.static("./public/images"));
 
-app
-	.get("/", (_req, res) => {
-	res
-		.status(200)
-		.send("WELCOME");
-})
+app.get("/", (_req, res) => {
+  res.status(200).send("WELCOME");
+});
 
-app.use('/', videos);
+app.use("/", videos);
 
 app.listen(PORT, () => {
-    console.log(`Server listening on ${BACKEND_URL}: ${PORT}`)
+  console.log(`Server listening on ${BACKEND_URL}: ${PORT}`);
 });
--- routes/videos.js
+++ routes/videos.js
@@ -1,66 +1,66 @@
 const express = require("express");
 const router = express.Router();
-const fs = require('fs');
-const { v4: uuid} = require("uuid");
-const bodyParser = require("body-parser")
+const fs = require("fs");
+const { v4: uuid } = require("uuid");
+const bodyParser = require("body-parser");
 
 require("dotenv").config();
 const { PORT, BACKEND_URL } = process.env;
 
 const videosFilePath = "./data/videos.json";
 
-router.use(bodyParser.json())
+router.use(bodyParser.json());
 
 const getVideos = () => {
-    return JSON.parse(fs.readFileSync(videosFilePath));
-}
+  return JSON.parse(fs.readFileSync(videosFilePath));
+};
 
-router.get('/videos', (_req, res) => {
-    let videoDetails = getVideos();
-    const videos = videoDetails.map((video) => {
-        const videoDetailsCondensed = {
-            title: video.title,
-            channel: video.channel,
-            image: video.image,
-            id: video.id
-        }
-        return videoDetailsCondensed;
-    })
-    res.send(videos);
+router.get("/videos", (_req, res) => {
+  let videoDetails = getVideos();
+  const videos = videoDetails.map((video) => {
+    const videoDetailsCondensed = {
+      title: video.title,
+      channel: video.channel,
+      image: video.image,
+      id: video.id,
+    };
+    return videoDetailsCondensed;
+  });
+  res.send(videos);
 });
 
-router.post('/videos', (req, res) => {
-    const { title, description } = req.body;
-    let videoDetails = getVideos();
-    const video = {
-        title: title,
-        channel: "Oliver's Adventures",
-        image: `${BACKEND_URL}:${PORT}/Upload-video-preview.jpg`,
-        description: description,
-        views: "0",
-        likes: "0",
-        duration: "10:14",
-        video: `${BACKEND_URL}:${PORT}/Upload-video-preview.jpg`,
-        timestamp: Date.now(),
-        comments: [],
-        id: uuid()
-    }
-  
-    videoDetails.push(video)
-    fs.writeFileSync("./data/videos.json", JSON.stringify(videoDetails))
-    res.send(video);
-})
+router.post("/videos", (req, res) => {
+  const { title, description } = req.body;
+  let videoDetails = getVideos();
+  const video = {
+    title: title,
+    channel: "Oliver's Adventures",
+    image: `${BACKEND_URL}:${PORT}/Upload-video-preview.jpg`,
+    description: description,
+    views: "0",
+    likes: "0",
+    duration: "10:14",
+    video: `${BACKEND_URL}:${PORT}/Upload-video-preview.jpg`,
+    timestamp: Date.now(),
+    comments: [],
+    id: uuid(),
+  };
 
-router.get('/videos/:id', (req, res) => {  
-    const requestedId = req.params.id;
-    let videos = getVideos();
-    const foundVideo = videos.find(video => video.id === requestedId);
+  videoDetails.push(video);
+  fs.writeFileSync("./data/videos.json", JSON.stringify(videoDetails));
+  res.send(video);
+});
 
-    if (!foundVideo) {
-        return res.status(404).send("Video with id " + requestedId + " not found");
-    }
-    
-    return res.send(foundVideo);
+router.get("/videos/:id", (req, res) => {
+  const requestedId = req.params.id;
+  let videos = getVideos();
+  const foundVideo = videos.find((video) => video.id === requestedId);
+
+  if (!foundVideo) {
+    return res.status(404).send("Video with id " + requestedId + " not found");
+  }
+
+  return res.send(foundVideo);
 });
 
 module.exports = router;
