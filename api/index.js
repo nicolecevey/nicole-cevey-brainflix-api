4b96245664c5f0664010accebe75006f038f2224
Nicole Cevey <nicole.cevey@gmail.com>
2024-02-09 15:54:53 -0500

    configuring index file

--- api/index.js
+++ api/index.js
@@ -13,14 +13,10 @@ app.use(cors());
 
 app.use(express.static("../public/images"));
 
-app.get("/api", (_req, res) => {
+app.get("/", (_req, res) => {
   res.setHeader('Content-Type', 'text/html');
   res.setHeader('Cache-Control', 's-max-age=1, stale-while-revalidate');
   res.status(200).send("WELCOME");
-});
-
-app.get("/", (_req, res) => {
-  res.send("Hello, this is the root path!");
 });
 
 app.use("/", videos);
